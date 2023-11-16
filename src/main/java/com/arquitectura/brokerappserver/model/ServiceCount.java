03a4d2aeb1a3537db3aca8354ad5287b7b594483
diegoalamilla <diego.alamilla123@gmail.com>
2023-11-16 15:28:19 -0600

    correcion de respuesta al cliente

--- src/main/java/com/arquitectura/brokerappserver/model/ServiceCount.java
+++ src/main/java/com/arquitectura/brokerappserver/model/ServiceCount.java
@@ -21,11 +21,11 @@ public class ServiceCount extends Service{
         answerBuilder.add("servicio","contar")
                         .add("respuestas",3)
                             .add("respuesta1",listProducts.get(0).getNombre())
-                                .add("valor1",String.valueOf(DAOProductos.getNumberofVotesFromProduct(listProducts.get(0).getNombre())))
+                                .add("valor1",DAOProductos.getNumberofVotesFromProduct(listProducts.get(0).getNombre()))
                                     .add("respuesta2",listProducts.get(1).getNombre())
-                                        .add("valor2",String.valueOf(DAOProductos.getNumberofVotesFromProduct(listProducts.get(1).getNombre())))
+                                        .add("valor2",DAOProductos.getNumberofVotesFromProduct(listProducts.get(1).getNombre()))
                                             .add("respuesta3",listProducts.get(2).getNombre())
-                                                .add("valor3",String.valueOf(DAOProductos.getNumberofVotesFromProduct(listProducts.get(2).getNombre())));
+                                                .add("valor3",DAOProductos.getNumberofVotesFromProduct(listProducts.get(2).getNombre()));
         JsonObject requestJSONObject = answerBuilder.build();
         return requestJSONObject.toString();
     }
